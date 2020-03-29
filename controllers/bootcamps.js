d788c8932db068318709abfcdd2559114214a77c
cherenkor <kucherenko.web@gmail.com>
2020-03-29 17:33:21 +0300

    Connect User with Bootcamp, Add users seeding

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,6 +3,7 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const geocoder = require("../utils/geocoder");
 const Bootcamp = require("../models/Bootcamp");
+const { admin } = require("../types/roles");
 
 // @decs    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -33,6 +34,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== admin) {
+    return next(
+      new ErrorResponse(
+        `User with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({
