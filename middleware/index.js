8ca2927e0d1bfba04e224bc684abfff90b92d3b8
cherenkor <kucherenko.web@gmail.com>
2020-03-31 23:37:15 +0300

    Add max requests per 10 min limit, Prevent http param pollution, Enable CORS potection

--- middleware/index.js
+++ middleware/index.js
@@ -6,6 +6,9 @@ const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 module.exports = app => {
   // Body parser
@@ -27,6 +30,20 @@ module.exports = app => {
 
   // Prevent XSS attacks
   app.use(xss());
+
+  // Rate limiting
+  app.use(
+    rateLimit({
+      windowMs: 600000, // 10 mins
+      max: 100 // max amount of request per 'windowMs' time
+    })
+  );
+
+  // Prevent http param pollution. For example: firstname=John&firstname=John -> ['John', 'John']
+  app.use(hpp());
+
+  // Enable CORS protection
+  app.use(cors());
 
   // File uploading
   app.use(fileupload());
